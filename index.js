9bf5acaa8b313712c7c9d3d4fefa164f3267b172
Naveen <inaveenraj22@gmail.com>
2019-09-08 00:17:05 +0530

    Added PUT delete Services

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.use(express.json());
 var courses =[
 {id :1,name :"Angular"},
 {id :2,name :"Node js"},
-{id :1,name:"Python"},
+{id :3,name:"Python"},
 ];
+
+//Getting the data for given Id
 
 app.get("/api/courses/:id",(req,res)=>
 {
@@ -18,8 +20,11 @@ app.get("/api/courses/:id",(req,res)=>
     res.send(course);
 
 });
+//Getting the data
+app.get('api1/courses',(req,res)=>{res.send(courses)});
+
 app.get('/',(req,res)=>{
-res.send("Hello World !!");
+res.send(courses);
 });
 app.get('/api/courses',(req,res)=>{
     res.send(["C#","Node Js","Angular"]);
@@ -29,6 +34,8 @@ app.get('/api/courses',(req,res)=>{
         res.send(req.params );
         });
     
+
+// Inserting Data
 
 app.post('/api/courses',(req,res)=>
 {
@@ -46,8 +53,43 @@ app.post('/api/courses',(req,res)=>
         }
 });
 
- 
+//Updating Data
 
+app.put('/:id',(req,res)=>
+{
+    //Checking Courses Id is Valid
+    var course = courses.find(x=>x.id == req.params.id)
+    if(!course){
+        res.status(404).send("Invalid Id");
+        return;       
+    } 
+    // Validating Provided Data is Correct
+    var schema ={name : Joi.string().min(3).required()}
+    var {error}= Joi.validate(req.body,schema);
+    if(error)
+    {
+    res.status(400).send(error.details[0].message);
+    return;
+    }
+      //Sucessful
+    course.name= req.body.name;
+    res.send(course);
+});
 
+//Deleting
+
+app.delete("/:id",(req,res)=>
+{
+
+    var course = courses.find(x=>x.id == req.params.id)
+    if(!course){
+        res.status(404).send("Invalid Id");
+        return;       
+    } 
+
+    var index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(course);
+});
         const port = process.env.PORT|| 3500;   
 app.listen(port,()=>console.log(`Application is listening.......${port}`))
